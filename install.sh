a7873947e3ecae3707b22c11bec938a64e6321b1
LyRicS001 <95881084+LyRicS001@users.noreply.github.com>
2022-01-21 17:51:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -43,15 +43,12 @@ install_options(){
 senha_ft() {
     output "Inserir Uma Senha Segura De Nivél 3:"
     read -r senhanv3
-    requerir_info
 }
 
 requerir_info() {
     output "Inserir Endereço De E-mail:"
     read -r email
-    dns_check
 }
-
 
 dns_check(){
     output "Colocar Seu Dominio/FQDN (panel.domain.tld):"
@@ -67,6 +64,7 @@ dns_check(){
         output "If you are using Cloudflare, please disable the orange cloud."
         output "If you do not have a domain, you can get a free one at https://freenom.com"
         dns_check
+        clear
     else
         output "Domain Correto. Continuando Trabalho..."
     fi
@@ -75,13 +73,13 @@ dns_check(){
 instalar_pacote1(){
     output "Atualizando Sistema/Pacote"
     apt-get update
+    clear
     apt upgrade -y
+    clear
     output "Instalando Dependencias De Pacotes"
     apt install -y apache2 apt-transport-https certbot python3-certbot-apache mysql-server php libapache2-mod-php php-mysql php-gd php-cli php-common php-mbstring php-ldap php-odbc php-pear php-xml php-xmlrpc php-bcmath php-pdo default-jdk git zip unzip
     output "Apache Mod. . ."
     a2enmod rewrite 
-
-    apache2 
 }
 
 apache2() {
@@ -96,9 +94,6 @@ apache2() {
 	ErrorLog ${APACHE_LOG_DIR}/error.log
 	CustomLog ${APACHE_LOG_DIR}/access.log combined
 </VirtualHost> " > multicraft.conf
-
-instalar_pacote2
-
 }
 
 instalar_pacote2(){
@@ -108,9 +103,6 @@ instalar_pacote2(){
     a2dissite 000-default.conf
     output "Apache Reiniciando..."
     systemctl restart apache2 
-
-    ssl_cerbot
-
 }
 
 ssl_cerbot(){
@@ -119,10 +111,10 @@ ssl_cerbot(){
     if [ "$installoption" = "2" ]; then
 	certbot --apache --redirect --no-eff-email --email "$email" --agree-tos -d "$FQDN"
     fi
-    instalar_painel
+    clear
 }
 
-instalar_painel() {
+database() {
     output "Gerando databases e refazendo root password..."
     password=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
     adminpassword=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`
@@ -132,13 +124,15 @@ instalar_painel() {
     Q2="GRANT ALL ON multicraft_panel.* TO 'multicraftpaneldbuser'@'localhost';"
     Q3="CREATE DATABASE multicraft_daemon CHARACTER SET utf8 COLLATE utf8_general_ci;"
     Q4="CREATE USER 'multicraftdaemondbuser'@'localhost' IDENTIFIED BY '${senhanv3}';"
-    Q5="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$rootpassword');"
-    Q6="GRANT ALL ON multicraft_daemon.* TO 'multicraftdaemondbuser'@'localhost';"
-    Q7="FLUSH PRIVILEGES;"
-    SQL="${Q0}${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}${Q7}"
+    Q5="GRANT ALL ON multicraft_daemon.* TO 'multicraftdaemondbuser'@'localhost';"
+    Q6="FLUSH PRIVILEGES;"
+    SQL="${Q0}${Q1}${Q2}${Q3}${Q4}${Q5}${Q6}"
     mysql -u root -e "$SQL"
+    clear
+}
 
-    output "Baixando Multicraft..."
+inst_painel(){
+        output "Baixando Multicraft..."
 
     wget http://www.multicraft.org/download/linux64 -O multicraft.tar.gz
     tar xvzf multicraft.tar.gz
@@ -147,14 +141,6 @@ instalar_painel() {
     output "Instalando Multicraft..."
  
     ./setup.sh
-
-}
-
-harden_linux(){
-    curl https://raw.githubusercontent.com/Whonix/security-misc/master/etc/modprobe.d/30_security-misc.conf >> /etc/modprobe.d/30_security-misc.conf
-    curl https://raw.githubusercontent.com/Whonix/security-misc/master/etc/sysctl.d/30_security-misc.conf >> /etc/sysctl.d/30_security-misc.conf
-    sed -i 's/kernel.yama.ptrace_scope=2/kernel.yama.ptrace_scope=3/g' /etc/sysctl.d/30_security-misc.conf
-    curl https://raw.githubusercontent.com/Whonix/security-misc/master/etc/sysctl.d/30_silent-kernel-printk.conf >> /etc/sysctl.d/30_silent-kernel-printk.conf
 }
 
 #Execution
@@ -162,7 +148,13 @@ preflight
 install_options
 case $installoption in 
     1)  senha_ft
-	    harden_linux
+        requerir_info
+        dns_check
         instalar_pacote1
+        apache2
+        instalar_pacote2
+        ssl_cerbot
+        database
+        inst_painel
         ;;
 esac
